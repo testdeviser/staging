b0575b13327f73758b0bbe57b960fcbaea30c867
rajuthakur888 <rajuthakur888now@gmail.com>
2023-06-02 11:49:08 +0530

    testfilechanges

---
+++ test.php
@@ -0,0 +1,20 @@
+git init
+git commit -m "first commit"
+git remote add origin https://github.com/testdeviser/staging.git
+git branch -M main
+git push -u origin main
+git remote add origin https://github.com/testdeviser/staging.git
+
+
+
+git status
+git add .
+git status
+git commit -m "test4"
+git push
+
+git branch -M main
+git push -u origin main
+
+
+
