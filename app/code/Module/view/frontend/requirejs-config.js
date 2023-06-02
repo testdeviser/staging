603d84cb617ca4dd74d748e717679f44aa1f0465
rajuthakur888 <rajuthakur888now@gmail.com>
2023-06-02 14:46:38 +0530

    deletemodule

--- app/code/Module/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Catalog/js/catalog-add-to-cart': {
-                'Custom_Module/js/catalog-add-to-cart-mixin': true
-            },
-        }
-    }
-};
